7f6bc0cba189b450cc9815781b33739f21668fff
orsowen <orsowenbenabdallah5@gmail.com>
2025-05-30 13:42:33 +0100

    hi

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 import { Capacitor } from '@capacitor/core';
 
 export const API_URL = Capacitor.getPlatform() === 'web'
-  ? 'http://localhost:3000/api'
-  : 'http://192.168.1.17:3000/api';
+  ? 'http://localhost:5000/api'
+  : 'http://192.168.1.17:5000/api';
 
   export const isMobile = Capacitor.getPlatform() !== 'web';
